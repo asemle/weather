0d20a4bb10ef2d869cf4cac98a1eebb02bcb8c4b
asemle <asemle@umass.edu>
2018-08-13 10:31:49 -0400

    set up redux

---
+++ src/ducks/getWeather.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const GET_WEATHER = 'GET_WEATHER';
+
+const initialState = {weather:[]}
+
+export function getWeather() {
+    const weather = axios.get(`https://swapi.co/api/people`)
+        .then((response) => { return response.data.results })
+
+    return {
+        type: GET_WEATHER,
+        payload: weather
+    }
+}
+
+export default function(state = initialState, action) {
+    switch(action.type) {
+        case 'GET_WEATHER_FULFILLED':
+        return Object.assign({}, state, {weather:action.payload})
+
+        default:
+            return state;
+    }
+}
